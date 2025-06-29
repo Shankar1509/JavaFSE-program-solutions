94018f3e32639f5310a00fc3ce4f3159f2e97107
Shankar1509 <shankarnallamuth04@gmail.com>
2025-06-29 22:01:18 +0530

    final commit

--- Week_1/1_Design_Patterns_and_principles/1_Implementing_Singleton_Pattern/Main.java
+++ Week_1/1_Design_Patterns_and_principles/1_Implementing_Singleton_Pattern/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         SingletonClass obj1 = SingletonClass.getInstance();
+        SingletonClass obj2 = SingletonClass.getInstance();
 
+        System.out.println(obj1.message);
+        System.out.println(obj2.message);
     }
 }
--- Week_1/1_Design_Patterns_and_principles/1_Implementing_Singleton_Pattern/SingletonClass.java
+++ Week_1/1_Design_Patterns_and_principles/1_Implementing_Singleton_Pattern/SingletonClass.java
@@ -1,6 +1,7 @@
 public class SingletonClass {
+    String message;
     private SingletonClass (){
-
+        this.message = "There is only one Class";
     }
     private static SingletonClass instance; //creating a ref variable for object
     public static SingletonClass getInstance(){
