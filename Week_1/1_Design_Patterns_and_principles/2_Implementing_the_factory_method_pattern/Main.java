76313f4336bbf4d9667adf4e9f8377813de35964
Shankar1509 <shankarnallamuth04@gmail.com>
2025-06-30 09:50:35 +0530

    final commit

--- Week_1/1_Design_Patterns_and_principles/2_Implementing_the_factory_method_pattern/Main.java
+++ Week_1/1_Design_Patterns_and_principles/2_Implementing_the_factory_method_pattern/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
         Factory factory1 = new Factory();
 
-        Shape s1 = new Shape();
+        Shape s1 =factory1.getShape("Circle");
         s1.draw();
 
-        Shape s2 = new Shape();
+        Shape s2 =factory1.getShape("Square") ;
         s2.draw();
     }
 }
