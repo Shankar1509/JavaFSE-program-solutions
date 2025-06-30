ad09705d0b21a35377080fbe143ee6b65c6cc642
Shankar1509 <shankarnallamuth04@gmail.com>
2025-06-30 09:49:06 +0530

    commit before output

---
+++ Week_1/1_Design_Patterns_and_principles/2_Implementing_the_factory_method_pattern/Factory.java
@@ -0,0 +1,10 @@
+public class Factory {
+    public Shape getShape(String shapeType) {
+        if (shapeType.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+        } else if (shapeType.equalsIgnoreCase("SQUARE")) {
+            return new Square();
+        }
+        return null;
+    }
+}
---
+++ Week_1/1_Design_Patterns_and_principles/2_Implementing_the_factory_method_pattern/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Factory factory1 = new Factory();
+
+        Shape s1 = new Shape();
+        s1.draw();
+
+        Shape s2 = new Shape();
+        s2.draw();
+    }
+}
