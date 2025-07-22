38f481a6f0e34800305ca23fcac885b2304d5c60
Shankar1509 <shankarnallamuth04@gmail.com>
2025-07-22 18:50:36 +0530

    commit

--- Week_1/1_Design_Patterns_and_principles/1_Implementing_Singleton_Pattern/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        SingletonClass obj1 = SingletonClass.getInstance();
-        SingletonClass obj2 = SingletonClass.getInstance();
-
-        System.out.println(obj1.message);
-        System.out.println(obj2.message);
-    }
-}
